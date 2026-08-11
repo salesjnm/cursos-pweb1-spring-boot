cc9b1825de7427be4f43b7f8c35e8015a25eb4b7
Janayna <salesjnm@gmail.com>
2026-08-11 18:32:49 -0300

    feat: adiciona InstrutorController e ajusta consulta derivada no InstrutorRepository

--- cursos_pweb1/src/main/java/com/janayna/cursos_pweb1/repositories/InstrutorRepository.java
+++ cursos_pweb1/src/main/java/com/janayna/cursos_pweb1/repositories/InstrutorRepository.java
@@ -1,14 +1,14 @@
 package com.janayna.cursos_pweb1.repositories;
 
-import org.springframework.stereotype.Repository;
-import org.springframework.data.jpa.repository.JpaRepository;
 import com.janayna.cursos_pweb1.entities.Instrutor;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface InstrutorRepository extends JpaRepository<Instrutor, Long> {
-    
-    //Consulta personalizada para buscar instrutores pelo nome
-    List<Instrutor> findByNameContaining(String nome);
+
+    // Altere "Name" para "Nome"
+    List<Instrutor> findByNomeContaining(String nome);
 }
