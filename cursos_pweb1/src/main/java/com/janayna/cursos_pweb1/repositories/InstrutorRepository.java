81a09d33708fde7496bfe5b68a95ff8d15f54050
Janayna <salesjnm@gmail.com>
2026-08-11 17:06:10 -0300

    feat: adiciona consulta derivada findByNomeContaining no InstrutorRepository

--- cursos_pweb1/src/main/java/com/janayna/cursos_pweb1/repositories/InstrutorRepository.java
+++ cursos_pweb1/src/main/java/com/janayna/cursos_pweb1/repositories/InstrutorRepository.java
@@ -4,7 +4,11 @@ import org.springframework.stereotype.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.janayna.cursos_pweb1.entities.Instrutor;
 
+import java.util.List;
+
 @Repository
 public interface InstrutorRepository extends JpaRepository<Instrutor, Long> {
     
+    //Consulta personalizada para buscar instrutores pelo nome
+    List<Instrutor> findByNameContaining(String nome);
 }
