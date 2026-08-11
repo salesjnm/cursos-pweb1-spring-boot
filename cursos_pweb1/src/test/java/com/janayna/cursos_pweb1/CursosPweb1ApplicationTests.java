acd9e45abbfbd1b9bb686a33e6146fd6c9e961a9
Janayna <salesjnm@gmail.com>
2026-08-11 15:25:09 -0300

    chore: estrutura inicial do projeto gerada com Spring Initializr

---
+++ cursos_pweb1/src/test/java/com/janayna/cursos_pweb1/CursosPweb1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.janayna.cursos_pweb1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursosPweb1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
